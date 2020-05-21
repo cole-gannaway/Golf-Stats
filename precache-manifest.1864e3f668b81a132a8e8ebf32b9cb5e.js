df7615cf7bca1c4a74da90f5a4e0d5e65e77ab32
Cole Gannaway <43042842+cole-gannaway@users.noreply.github.com>
2020-05-21 00:21:12 -0400

    Updates

---
+++ precache-manifest.1864e3f668b81a132a8e8ebf32b9cb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a61c0d6563f1fa0bb32df61349d4e5",
+    "url": "/Golf-Stats/index.html"
+  },
+  {
+    "revision": "f8a988a7a59df6fae18d",
+    "url": "/Golf-Stats/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "86b1b54cebc40e62bd1a",
+    "url": "/Golf-Stats/static/js/2.deeefd32.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Golf-Stats/static/js/2.deeefd32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a988a7a59df6fae18d",
+    "url": "/Golf-Stats/static/js/main.6a77b433.chunk.js"
+  },
+  {
+    "revision": "3e338f2c92062893f623",
+    "url": "/Golf-Stats/static/js/runtime-main.c6a92872.js"
+  }
+]);
